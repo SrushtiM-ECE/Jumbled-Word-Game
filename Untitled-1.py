eeade5841970b30b7ea92df4d07dced96c4ecf84
SRUSHTI M <srushtim1120@gmail.com>
2026-02-19 23:05:17 +0530

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,49 +1,9 @@
-class BankAccount:
-    def __init__(self, name, balance=0):
-        self.name = name
-        self.balance = balance
+word = input("Enter a word: ")
+vowels = "aeiouAEIOU"
+count = 0
 
-    def deposit(self, amount):
-        self.balance += amount
-        print(f"Deposited: {amount}")
+for ch in word:
+    if ch in vowels:
+        count += 1
 
-    def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient balance!")
-        else:
-            self.balance -= amount
-            print(f"Withdrawn: {amount}")
-
-    def show_balance(self):
-        print(f"Current Balance: {self.balance}")
-
-print("=== Bank Account Simulator ===")
-
-name = input("Enter account holder name: ")
-account = BankAccount(name)
-
-while True:
-    print("\n1. Deposit")
-    print("2. Withdraw")
-    print("3. Balance")
-    print("4. Exit")
-
-    choice = input("Choose option: ")
-
-    if choice == "1":
-        amt = float(input("Enter amount: "))
-        account.deposit(amt)
-
-    elif choice == "2":
-        amt = float(input("Enter amount: "))
-        account.withdraw(amt)
-
-    elif choice == "3":
-        account.show_balance()
-
-    elif choice == "4":
-        print("Thank you!")
-        break
-
-    else:
-        print("Invalid choice")
+print("Number of vowels:", count)
